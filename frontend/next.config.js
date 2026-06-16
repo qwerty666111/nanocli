10f481a0ada8645df2c105b66f6eca02aef38969
RoninBuild <zmsmaxim@gmail.com>
2026-06-16 13:40:32 +0500

    fix: remove webpack aliases causing missing chunk error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@react-native-async-storage/async-storage": false,
-      "pino-pretty": false,
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
